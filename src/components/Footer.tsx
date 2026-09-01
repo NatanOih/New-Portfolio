b422e76a708ffa3e219ebd9bf971fed98cc49ae0
NatanOih <natanoih@gmail.com>
2026-09-01 12:49:59 +0300

    Make footer copyright year dynamic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="pb-10 px-4 text-center text-gray-500">
       <small className="text-xs block pb-2">
-        &copy; 2023 Natan Oihman. All rights reserved.
+        &copy; {currentYear} Natan Oihman. All rights reserved.
       </small>
     </footer>
   );
